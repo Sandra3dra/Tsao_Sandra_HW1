02a81e6b55721a7b6d1e8ab1a02dbbf29d958929
SandraTsao <s_tsao@fanshaweonline.com>
2019-10-14 01:11:26 -0400

    style 3

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,18 @@
     // test fired
     console.log('fired!');
 
-    // window.addEventListener('scroll', function() {
-    //     var pagePosition = pageYOffset + "px";
-    //     console.log(pagePosition);
-    // });
-    var windowPosition = pageYOffset;
-    var pagePosition = pageYOffset + "px";
-    window.addEventListener('wheel', function() {
-        console.log(pagePosition);
+    var galleries = document.querySelectorAll(".galleries");
+
+    galleries.forEach(gallery => {
+        gallery.addEventListener("click", function() {
+                // var h5 = document.getElementById("h5");
+                // h5.classList.add("hidden");
+                let newImg = `<img class="gallery-image" src="images/${"g" + this.id}.jpg" alt="image gallery">`;
+                var showHere = document.getElementById("showHere");
+                showHere.innerHTML = "";
+                showHere.innerHTML += newImg;
+            console.log("you are browsing images from gallery!");
+        });
     });
+    
 })()
